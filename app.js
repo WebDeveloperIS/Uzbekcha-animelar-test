588f3281ad18ce0545c9bcbf28d235f23244fbc6
Samandar <samandari862@gmail.com>
2026-02-01 02:11:17 +0500

    fix duplicate path

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.use(session({
 
 // Multer
 const multer = require("multer");
-const path = require("path");
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/uploads");
