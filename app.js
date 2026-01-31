05d7699eb9b12bfadab0248d8af3775530991882
Samandar <samandari862@gmail.com>
2026-02-01 02:22:39 +0500

    fix upload route

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.use(session({
 
 // Multer
 const multer = require("multer");
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/uploads");
+    cb(null, path.join(__dirname, "public/uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
